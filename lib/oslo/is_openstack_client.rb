6869175017b1c37343977df5cd74b3de8bdb0f52
Uchio KONDO <udzura@pepabo.com>
2015-08-21 20:07:14 +0900

    Const reassign hack...

--- lib/oslo/is_openstack_client.rb
+++ lib/oslo/is_openstack_client.rb
@@ -3,6 +3,12 @@ unless defined? OpenStack
   end
 
   Oslo = OpenStack
+
+  # XXX: Some case, 'oslo/version' is pre-required and
+  # will delete Oslo::VERSION const by reassign in L5
+  unless require('oslo/version')
+    load 'oslo/version.rb'
+  end
 end
 
 require 'oslo'
