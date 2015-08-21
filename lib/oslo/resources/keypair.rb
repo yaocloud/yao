471e4deb561a5eda682a8d6592fffc75848e124e
Uchio KONDO <udzura@pepabo.com>
2015-08-21 18:45:28 +0900

    Keypair JSON has weird structure

--- lib/oslo/resources/keypair.rb
+++ lib/oslo/resources/keypair.rb
@@ -3,5 +3,9 @@ module Oslo::Resources
     self.service        = "compute"
     self.resource_name  = "os-keypair"
     self.resources_name = "os-keypairs"
+
+    def self.list
+      super.map{|r| r[resource_name_in_json] }
+    end
   end
 end
