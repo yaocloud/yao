23b7934642b8c409cce0dee2fde5057448505784
Uchio KONDO <udzura@pepabo.com>
2015-08-21 16:03:47 +0900

    Add CUD methods of CRUD

--- lib/oslo/resources/restful_resources.rb
+++ lib/oslo/resources/restful_resources.rb
@@ -8,13 +8,14 @@ module Oslo::Resources
 
       extend Forwardable
       %w(get post put delete).each do |method_name|
-          def_delegator :client, method_name, method_name.upcase
+        def_delegator :client, method_name, method_name.upcase
       end
 
       def service=(name)
         @client ||= Oslo.default_client.pool[name]
       end
 
+      # restful methods
       def list
         GET(resources_name).body[resources_name]
       end
@@ -30,6 +31,34 @@ module Oslo::Resources
                 GET([resources_name, id_or_permalink].join("/"))
               end
         res.body[resource_name]
+      end
+      alias find get
+
+      def create(resource_params)
+        params = {
+          resource_name => resource_params
+        }
+        res = POST(resources_name) do |req|
+          req.body = params.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+        res.body[resource_name]
+      end
+
+      def update(id, resource_params)
+        params = {
+          resource_name => resource_params
+        }
+        res = PUT([resources_name, id].join("/")) do |req|
+          req.body = params.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+        res.body[resource_name]
+      end
+
+      def destroy(id)
+        res = DELETE([resources_name, id].join("/"))
+        res.body
       end
     end
   end
