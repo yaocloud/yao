2105321802ff6ae51e6760df4c122ffea5fa429b
Uchio KONDO <udzura@pepabo.com>
2015-08-21 16:29:38 +0900

    We set missing const name

--- lib/oslo/resources.rb
+++ lib/oslo/resources.rb
@@ -6,4 +6,8 @@ module Oslo
     autoload :Flavor,           "oslo/resources/flavor"
     autoload :Image,            "oslo/resources/image"
   end
+
+  def self.const_missing(name)
+    Oslo.const_set(name, Oslo::Resources.const_get(name))
+  end
 end
