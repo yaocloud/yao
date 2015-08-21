dd0c9c4ef8aa0275342606b6829d4fe9ac61d5d3
Uchio KONDO <udzura@pepabo.com>
2015-08-21 16:09:30 +0900

    Fill the difference btw URI and json path

--- lib/oslo/resources/restful_resources.rb
+++ lib/oslo/resources/restful_resources.rb
@@ -17,11 +17,11 @@ module Oslo::Resources
 
       # restful methods
       def list
-        GET(resources_name).body[resources_name]
+        GET(resources_name).body[resources_name_in_json]
       end
 
       def list_detail
-        GET([resources_name, "detail"].join("/")).body[resources_name]
+        GET([resources_name, "detail"].join("/")).body[resources_name_in_json]
       end
 
       def get(id_or_permalink)
@@ -30,35 +30,44 @@ module Oslo::Resources
               else
                 GET([resources_name, id_or_permalink].join("/"))
               end
-        res.body[resource_name]
+        res.body[resource_name_in_json]
       end
       alias find get
 
       def create(resource_params)
         params = {
-          resource_name => resource_params
+          resource_name_in_json => resource_params
         }
         res = POST(resources_name) do |req|
           req.body = params.to_json
           req.headers['Content-Type'] = 'application/json'
         end
-        res.body[resource_name]
+        res.body[resource_name_in_json]
       end
 
       def update(id, resource_params)
         params = {
-          resource_name => resource_params
+          resource_name_in_json => resource_params
         }
         res = PUT([resources_name, id].join("/")) do |req|
           req.body = params.to_json
           req.headers['Content-Type'] = 'application/json'
         end
-        res.body[resource_name]
+        res.body[resource_name_in_json]
       end
 
       def destroy(id)
         res = DELETE([resources_name, id].join("/"))
         res.body
+      end
+
+      private
+      def resource_name_in_json
+        @resource_name_in_json  ||= resource_name.sub(/^os-/, "").tr("-", "_")
+      end
+
+      def resources_name_in_json
+        @resources_name_in_json ||= resources_name.sub(/^os-/, "").tr("-", "_")
       end
     end
   end
