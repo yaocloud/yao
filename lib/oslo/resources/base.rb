61fe9b995cf6d1da320f889d47478ddd729c770a
Uchio KONDO <udzura@pepabo.com>
2015-08-25 15:11:43 +0900

    Add resource struct definition macros
    - example is Resources::Server, which behaves nicely

--- lib/oslo/resources/base.rb
+++ lib/oslo/resources/base.rb
@@ -1,4 +1,6 @@
 require 'oslo/resources/restfully_accessible'
+require 'time'
+
 module Oslo::Resources
   class Base
     def self.friendly_attributes(*names)
@@ -9,12 +11,47 @@ module Oslo::Resources
       end
     end
 
+    def self.map_attribute_to_attribute(k_and_v)
+      as_json, as_method = *k_and_v.to_a.first.map(&:to_s)
+      define_method(as_method) do
+        self[as_json]
+      end
+    end
+
+    def self.map_attribute_to_resource(k_and_v)
+      _name, klass = *k_and_v.to_a.first
+      name = _name.to_s
+      define_method(name) do
+        self[[name, klass].join("__")] ||= klass.new(self[name])
+      end
+    end
+
     def initialize(data_via_json)
       @data = data_via_json
     end
 
     def [](name)
       @data[name]
+    end
+
+    def []=(name, value)
+      @data[name] = value
+    end
+
+    def id
+      self["id"]
+    end
+
+    def created
+      if date = self["created"]
+        Time.parse(date)
+      end
+    end
+
+    def updated
+      if date = self["updated"]
+        Time.parse(date)
+      end
     end
 
     extend RestfullyAccessible
--- lib/oslo/resources/server.rb
+++ lib/oslo/resources/server.rb
@@ -1,5 +1,12 @@
 module Oslo::Resources
   class Server < Base
+    friendly_attributes :addresses, :metadata, :name, :progress,
+                        :status, :tenant_id, :user_id, :key_name
+    map_attribute_to_attribute :hostId => :host_id
+    map_attribute_to_resource  :flavor => Flavor
+    map_attribute_to_resource  :image  => Image
+    map_attribute_to_resources :security_groups => SecurityGroup
+
     self.service        = "compute"
     self.resource_name  = "server"
     self.resources_name = "servers"
