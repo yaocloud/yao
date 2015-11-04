71953e20a67290284353c2d3443b470f4a297b84
Joe-noh <honzawa.j@pepabo.com>
2015-11-04 15:14:37 +0900

    add two methods to Tenant
    - get_by_name: get sngle tenant information by name
    - granted: get tenants that the logged in user can access

--- lib/yao/resources/tenant.rb
+++ lib/yao/resources/tenant.rb
@@ -6,5 +6,20 @@ module Yao::Resources
     self.resource_name  = "tenant"
     self.resources_name = "tenants"
     self.admin          = true
+
+    class << self
+      def get_by_name(name)
+        self.get("", name: name)
+      end
+      alias find_by_name get_by_name
+
+      def granted
+        @admin = false
+        tenants = self.list
+        @admin = true
+
+        tenants
+      end
+    end
   end
 end
