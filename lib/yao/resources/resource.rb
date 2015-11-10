22aaf9705c770fa6f4a8b83b4c7699e1481a235f
Uchio KONDO <udzura@pepabo.com>
2015-11-10 19:54:02 +0900

    Add basic attributes

--- lib/yao/resources/resource.rb
+++ lib/yao/resources/resource.rb
@@ -1,5 +1,30 @@
+require 'time'
 module Yao::Resources
   class Resource < Base
+    friendly_attributes :user_id, :resource_id, :project_id,
+                        :last_sample_timestamp, :first_sample_timestamp,
+                        :metadata,
+                        :links
+
+    def id
+      resource_id
+    end
+
+    def tenant
+      @tenant ||= Yap::User.get(project_id)
+    end
+
+    def user
+      @user ||= Yap::User.get(user_id)
+    end
+
+    def last_sampled_at
+      Time.parse last_sample_timestamp
+    end
+
+    def first_sampled_at
+      Time.parse first_sample_timestamp
+    end
 
     self.service        = "metering"
     self.api_version    = "v2"
