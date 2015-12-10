30f679934dfdde10dc5d1ebcadf25b8b27a569d5
ku00 <skentaro36@gmail.com>
2015-12-10 16:35:09 +0900

    add attr tests of `user` and `role`
    those resources is used in Kaname

---
+++ test/yao/resources/test_role.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class TestRole < Test::Unit::TestCase
+  def test_role_attributes
+    params = {
+      "name" => "test_role",
+      "description" => "test_description_1"
+    }
+
+    role = Yao::Role.new(params)
+    assert_equal(role.name, "test_role")
+    assert_equal(role.description, "test_description_1")
+  end
+end
