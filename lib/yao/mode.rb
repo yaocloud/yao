ed7522cd21376417ff9da13871dd363ad6aff2fc
Joe-noh <goflb.jh@gmail.com>
2016-01-10 16:52:50 +0900

    reentrant-safe

--- lib/yao/mode.rb
+++ lib/yao/mode.rb
@@ -3,22 +3,26 @@ module Yao
     def read_only!(&blk)
       raise unless block_given?
 
+      before = Yao.config.raise_on_write
+
       Yao.config.set :raise_on_write, true
       begin
         yield
       ensure
-        Yao.config.set :raise_on_write, false
+        Yao.config.set :raise_on_write, before
       end
     end
 
     def read_only(&blk)
       raise unless block_given?
 
+      before = Yao.config.noop_on_write
+
       Yao.config.set :noop_on_write, true
       begin
         yield
       ensure
-        Yao.config.set :noop_on_write, false
+        Yao.config.set :noop_on_write, before
       end
     end
   end
