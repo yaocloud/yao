f37fad7f781b5c2ddb27ecbef67c95aa5b4f09f3
Yuki Koya <ykky@pepabo.com>
2017-08-01 20:22:19 +0900

    fix typo

--- lib/yao/resources/restfully_accessible.rb
+++ lib/yao/resources/restfully_accessible.rb
@@ -95,7 +95,7 @@ module Yao::Resources
     def get(id_or_permalink, query={})
       res = if id_or_permalink =~ /^https?:\/\//
               GET(id_or_permalink, query)
-            elsif is_uuid?(id_or_name_or_permalink)
+            elsif is_uuid?(id_or_permalink)
               GET([resources_path, id_or_permalink].join("/"), query)
             else
               find_by_name(id_or_permalink, query)
