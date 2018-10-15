fd04db48d61b0222d7d51d0cecad3a44c36646cb
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2018-10-15 15:44:28 +0900

    add test

---
+++ test/yao/resources/test_volume.rb
@@ -0,0 +1,15 @@
+require "time"
+require "date"
+
+class TestRole < Test::Unit::TestCase
+  def test_volume
+    params = {
+        'name' => 'cinder',
+        'size' => 10
+    }
+
+    volume = Yao::Volume.new(params)
+    assert_equal('cinder', volume.name)
+    assert_equal(10, volume.size)
+  end
+end
