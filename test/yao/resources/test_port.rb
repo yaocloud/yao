a70a451de44fa41bd82d752b66ea8a84d28b32d4
hiboma <hito@pepabo.com>
2019-08-09 12:33:03 +0900

    add test of Yao::Port#teant

--- test/yao/resources/test_port.rb
+++ test/yao/resources/test_port.rb
@@ -1,4 +1,9 @@
 class TestPort < Test::Unit::TestCase
+
+  def setup
+    Yao.default_client.pool["compute"] = Yao::Client.gen_client("https://example.com:12345")
+  end
+
   def test_port
 
     # https://docs.openstack.org/api-ref/network/v2/?expanded=list-floating-ips-detail,show-port-details-detail#ports
@@ -71,5 +76,24 @@ class TestPort < Test::Unit::TestCase
 
     # map_attribute_to_attribute
     assert_equal(port.host_id, "compute-000")
+  end
+
+  def test_port_to_tenant
+
+    stub_request(:get, "http://endpoint.example.com:12345/v2.0/tenants/0123456789abcdef0123456789abcdef")
+      .to_return(
+        status: 200,
+        body: <<-JSON,
+        {
+          "tenants": [{
+            "id": "0123456789abcdef0123456789abcdef"
+          }]
+        }
+        JSON
+        headers: {'Content-Type' => 'application/json'}
+      )
+
+    port = Yao::Port.new('tenant_id' => '0123456789abcdef0123456789abcdef')
+    assert { port.tenant.is_a? Yao::Tenant }
   end
 end
