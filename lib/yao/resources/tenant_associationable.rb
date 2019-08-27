7332bbc4b4894246b197245e0effc130d4c2ca95
hiboma <hito@pepabo.com>
2019-08-27 14:55:47 +0900

    Yao::Resources::FloatingIP を TenantAssociationable でリファクタリングする project への alias を足した

--- lib/yao/resources/tenant_associationable.rb
+++ lib/yao/resources/tenant_associationable.rb
@@ -4,11 +4,14 @@ module Yao
 
       def self.included(base)
         base.friendly_attributes :tenant_id
+        base.friendly_attributes :project_id
       end
 
       def tenant
         Yao::Tenant.find tenant_id
       end
+
+      alias :project :tenant
     end
   end
 end
