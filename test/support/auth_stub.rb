565f08aa938f072ad29b8a6a6bff7a0100e5392e
hiboma <hito@pepabo.com>
2019-08-28 12:43:24 +0900

    Yao::Resources::* のテストでクライアントの生成を記述せずにすrむように stub するヘルパーを足す

--- test/support/auth_stub.rb
+++ test/support/auth_stub.rb
@@ -1,4 +1,25 @@
 module AuthStub
+
+  # Yao::Resources::* のテストで Yao.default.pool の設定を都度都度 記述しなくていいよういするヘルパー
+  # endpoint の URL がマチマチだとテストを記述するのが面倒なので example.com で統一している
+  def stub_client
+    auth_url = "http://example.com:12345"
+    username = "yao"
+    tenant   = "default"
+    password = "password"
+
+    stub_auth_request(auth_url, username, password, tenant)
+
+    Yao.config.set :auth_url, auth_url
+    Yao::Auth.new(tenant_name: tenant, username: username, password: password)
+
+    client = Yao::Client.gen_client("https://example.com:12345")
+    Yao.default_client.admin_pool["identity"] = client
+    Yao.default_client.pool["network"]        = client
+    Yao.default_client.pool["compute"]        = client
+    Yao.default_client.pool["metering"]       = client
+  end
+
   def stub_auth_request(auth_url, username, password, tenant)
     stub_request(:post, "#{auth_url}/tokens")
       .with(
