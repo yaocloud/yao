287175b38380836491faeefe3c727f4e79663164
hiboma <hito@pepabo.com>
2019-08-29 19:29:23 +0900

    refactoring: rename resource -> sample beacuse these are test of Yao::Resources::Sample

--- test/yao/resources/test_sample.rb
+++ test/yao/resources/test_sample.rb
@@ -25,25 +25,25 @@ class TestSample < Test::Unit::TestCase
       "volume" => 1.0
     }
 
-    resource = Yao::Resources::Sample.new(params)
-    assert_equal(resource.id, "2e589cbc-738f-11e9-a9b2-bc764e200515")
-    assert_equal(resource.metadata, {
+    sample = Yao::Resources::Sample.new(params)
+    assert_equal(sample.id, "2e589cbc-738f-11e9-a9b2-bc764e200515")
+    assert_equal(sample.metadata, {
       "name1" => "value1",
       "name2" => "value2"
     })
-    assert_equal(resource.meter, "instance")
-    assert_equal(resource.source, "openstack")
-    assert_equal(resource.type, "gauge")
-    assert_equal(resource.unit, "instance")
-    assert_equal(resource.volume, 1.0)
-    assert_equal(resource.resource_id, "bd9431c1-8d69-4ad3-803a-8d4a6b89fd36")
-    assert_equal(resource.user_id, "efd87807-12d2-4b38-9c70-5f5c2ac427ff")
+    assert_equal(sample.meter, "instance")
+    assert_equal(sample.source, "openstack")
+    assert_equal(sample.type, "gauge")
+    assert_equal(sample.unit, "instance")
+    assert_equal(sample.volume, 1.0)
+    assert_equal(sample.resource_id, "bd9431c1-8d69-4ad3-803a-8d4a6b89fd36")
+    assert_equal(sample.user_id, "efd87807-12d2-4b38-9c70-5f5c2ac427ff")
 
-    assert_equal(resource.recorded_at.to_s, "2015-01-01 12:00:00 +0900")
-    assert_instance_of(Time, resource.recorded_at)
+    assert_equal(sample.recorded_at.to_s, "2015-01-01 12:00:00 +0900")
+    assert_instance_of(Time, sample.recorded_at)
 
-    assert_equal(resource.timestamp.to_s, "2015-01-01 12:00:00 +0900")
-    assert_instance_of(Time, resource.timestamp)
+    assert_equal(sample.timestamp.to_s, "2015-01-01 12:00:00 +0900")
+    assert_instance_of(Time, sample.timestamp)
   end
 
   def test_resource
