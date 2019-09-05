c73048534b95b17b8695ccce05ea2a689de732dd
Yuki Koya <ykky@pepabo.com>
2019-09-05 18:13:37 +0900

    v2.0からv3に変更したときに期待した動作にならないので修正

--- lib/yao/resources/user.rb
+++ lib/yao/resources/user.rb
@@ -9,21 +9,22 @@ module Yao::Resources
     self.resources_name = "users"
     self.admin          = true
 
-    def self.keystone_v2?
-      self.client.url_prefix.to_s.match?(/v2\.0/)
-    end
-
-    if self.keystone_v2?
-      self.return_single_on_querying = true
-    end
-
     class << self
       def find_by_name(name, query={})
-        if keystone_v2?
+        if api_verion_v2?
           [super]
         else
           super
         end
+      end
+
+      def return_single_on_querying
+        api_verion_v2?
+      end
+
+      private
+      def api_verion_v2?
+        client.url_prefix.to_s.match?(/v2\.0/)
       end
     end
   end
