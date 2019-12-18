5302a8e13c7cdb17149dac670beb19b3f35e8ff9
r_takaishi <ryo.takaishi.0@gmail.com>
2019-12-18 16:43:27 +0900

    support amphora api

---
+++ lib/yao/resources/amphora.rb
@@ -0,0 +1,13 @@
+module Yao::Resources
+  class Amphora < Base
+    friendly_attributes :loadbalancer_id, :compute_id, :lb_network_ip, :vrrp_ip, :ha_ip, :vrrp_port_id,
+                        :ha_port_id, :role, :status, :vrrp_interface, :vrrp_id,
+                        :vrrp_priority, :cached_zone, :image_id, :compute_flavor
+
+    self.service        = "load-balancer"
+    self.api_version    = "v2"
+    self.resource_name  = "amphorae"
+    self.resources_name = "amphorae"
+    self.resources_path = "octavia/amphorae"
+  end
+end
