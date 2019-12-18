7fea4b04a558128ab9a1148a48bcdda17293fe6d
r_takaishi <ryo.takaishi.0@gmail.com>
2019-12-18 14:38:37 +0900

    fix more typo

--- test/yao/resources/test_loadbalancer.rb
+++ test/yao/resources/test_loadbalancer.rb
@@ -6,7 +6,7 @@ class TestLoadBalancer < TestYaoResource
       "admin_state_up" => true,
       "provisioning_status" => "ACTIVE",
       "vip_address" => "198.51.100.1",
-      "operationg_status" => "ONLINE",
+      "operating_status" => "ONLINE",
       "name" => "greate loadbalancer",
       "created_at" => "2017-02-28T00:41:44",
       "updated_at" => "2017-02-28T00:43:30",
@@ -18,7 +18,7 @@ class TestLoadBalancer < TestYaoResource
     assert_equal(true, lb.admin_state_up)
     assert_equal("ACTIVE", lb.provisioning_status)
     assert_equal("198.51.100.1", lb.vip_address)
-    assert_equal("ONLINE", lb.operationg_status)
+    assert_equal("ONLINE", lb.operating_status)
     assert_equal("greate loadbalancer", lb.name)
     assert_equal(Time.parse("2017-02-28T00:41:44"), lb.created)
     assert_equal(Time.parse("2017-02-28T00:43:30"), lb.updated)
