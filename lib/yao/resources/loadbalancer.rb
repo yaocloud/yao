38c7c06f39883638357476125ca358e416a7838d
r_takaishi <ryo.takaishi.0@gmail.com>
2019-12-19 11:57:10 +0900

    invalid mapping.

--- lib/yao/resources/loadbalancer.rb
+++ lib/yao/resources/loadbalancer.rb
@@ -4,7 +4,7 @@ module Yao::Resources
                         :vip_address, :operating_status, :name
 
     map_attribute_to_resources listeners: LoadBalancerListener
-    map_attribute_to_resources pools: LoadBalancerListener
+    map_attribute_to_resources pools: LoadBalancerPool
 
     def project
       if project_id = self["project_id"]
