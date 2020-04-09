3a3cf13870dbd15c711c07ccd66a8fbb4acfb23d
n01e0 <noleo@vanillabeans.mobi>
2020-04-09 13:00:23 +0900

    dry

--- test/yao/resources/test_image.rb
+++ test/yao/resources/test_image.rb
@@ -1,43 +1,42 @@
 class TestImage < TestYaoResource
+    # https://docs.openstack.org/api-ref/compute/?expanded=list-flavors-detail,list-flavors-with-details-detail,list-hosts-detail,show-host-details-detail,list-images-detail,list-images-with-details-detail#list-images-with-details
+  @@params = {
+    "OS-DCF:diskConfig" => "AUTO",
+    "OS-EXT-IMG-SIZE:size" => 74185822,
+    "created" => "2011-01-01T01:02:03Z",
+    "id" => "70a599e0-31e7-49b7-b260-868f441e862b",
+    "links" => [
+      {
+        "href" => "http://openstack.example.com/v2/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
+        "rel" => "self"
+      },
+      {
+        "href" => "http://openstack.example.com/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
+        "rel" => "bookmark"
+      },
+      {
+        "href" => "http://glance.openstack.example.com/images/70a599e0-31e7-49b7-b260-868f441e862b",
+        "rel" => "alternate",
+        "type" => "application/vnd.openstack.image"
+      }
+    ],
+    "metadata" => {
+      "architecture" => "x86_64",
+      "auto_disk_config" => "True",
+      "kernel_id" => "nokernel",
+      "ramdisk_id" => "nokernel"
+    },
+    "minDisk" => 0,
+    "minRam" => 0,
+    "name" => "fakeimage7",
+    "progress" => 100,
+    "status" => "ACTIVE",
+    "updated" => "2011-01-01T01:02:03Z"
+  }
 
   def test_image
 
-    # https://docs.openstack.org/api-ref/compute/?expanded=list-flavors-detail,list-flavors-with-details-detail,list-hosts-detail,show-host-details-detail,list-images-detail,list-images-with-details-detail#list-images-with-details
-    params = {
-      "OS-DCF:diskConfig" => "AUTO",
-      "OS-EXT-IMG-SIZE:size" => 74185822,
-      "created" => "2011-01-01T01:02:03Z",
-      "id" => "70a599e0-31e7-49b7-b260-868f441e862b",
-      "links" => [
-        {
-          "href" => "http://openstack.example.com/v2/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "self"
-        },
-        {
-          "href" => "http://openstack.example.com/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "bookmark"
-        },
-        {
-          "href" => "http://glance.openstack.example.com/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "alternate",
-          "type" => "application/vnd.openstack.image"
-        }
-      ],
-      "metadata" => {
-        "architecture" => "x86_64",
-        "auto_disk_config" => "True",
-        "kernel_id" => "nokernel",
-        "ramdisk_id" => "nokernel"
-      },
-      "minDisk" => 0,
-      "minRam" => 0,
-      "name" => "fakeimage7",
-      "progress" => 100,
-      "status" => "ACTIVE",
-      "updated" => "2011-01-01T01:02:03Z"
-    }
-
-    image = Yao::Resources::Image.new(params)
+    image = Yao::Resources::Image.new(@@params)
 
     # friendly_attributes
     assert_equal("fakeimage7", image.name)
@@ -63,42 +62,7 @@ class TestImage < TestYaoResource
 
   def test_resource_from_json
 
-    # https://docs.openstack.org/api-ref/compute/?expanded=list-flavors-detail,list-flavors-with-details-detail,list-hosts-detail,show-host-details-detail,list-images-detail,list-images-with-details-detail#list-images-with-details
-    params = {
-      "OS-DCF:diskConfig" => "AUTO",
-      "OS-EXT-IMG-SIZE:size" => 74185822,
-      "created" => "2011-01-01T01:02:03Z",
-      "id" => "70a599e0-31e7-49b7-b260-868f441e862b",
-      "links" => [
-        {
-          "href" => "http://openstack.example.com/v2/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "self"
-        },
-        {
-          "href" => "http://openstack.example.com/6f70656e737461636b20342065766572/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "bookmark"
-        },
-        {
-          "href" => "http://glance.openstack.example.com/images/70a599e0-31e7-49b7-b260-868f441e862b",
-          "rel" => "alternate",
-          "type" => "application/vnd.openstack.image"
-        }
-      ],
-      "metadata" => {
-        "architecture" => "x86_64",
-        "auto_disk_config" => "True",
-        "kernel_id" => "nokernel",
-        "ramdisk_id" => "nokernel"
-      },
-      "minDisk" => 0,
-      "minRam" => 0,
-      "name" => "fakeimage7",
-      "progress" => 100,
-      "status" => "ACTIVE",
-      "updated" => "2011-01-01T01:02:03Z"
-    }
-
-    image = Yao::Resources::Image.send(:resource_from_json, params)
+    image = Yao::Resources::Image.send(:resource_from_json, @@params)
 
     # friendly_attributes
     assert_equal("fakeimage7", image.name)
