dc455b84ea892176d86a3d481e466d29dc3a51b5
Yuki Koya <ykky@pepabo.com>
2020-07-28 16:05:28 +0900

    Add friendly methods

--- lib/yao/resources/volume.rb
+++ lib/yao/resources/volume.rb
@@ -1,12 +1,23 @@
 module Yao::Resources
   class Volume < Base
-    friendly_attributes :name, :size, :volume_type
+    friendly_attributes :name, :size, :volume_type, :attachments, :availability_zone,
+                        :description, :migration_status, :metadata, :status,
+                        :volume_image_metadata
 
+    map_attribute_to_attribute 'encrypted' => :encrypted?
+    map_attribute_to_attribute 'multiattach' => :multiattach?
     map_attribute_to_attribute 'os-vol-tenant-attr:tenant_id' => :tenant_id
+    map_attribute_to_attribute 'os-vol-host-attr:host' => :host
+
+    alias :type :volume_type
 
     self.service        = "volumev3"
     self.resource_name  = "volume"
     self.resources_name = "volumes"
     self.resources_detail_available = true
+
+    def bootable?
+      self["bootable"] == "true" ? true : false
+    end
   end
 end
--- test/yao/resources/test_volume.rb
+++ test/yao/resources/test_volume.rb
@@ -1,13 +1,59 @@
 class TestVolume < TestYaoResource
   def test_volume
+    # https://docs.openstack.org/api-ref/block-storage/v3/index.html?expanded=show-a-volume-s-details-detail#show-a-volume-s-details
     params = {
-        'name' => 'cinder',
-        'size' => 10
+      "attachments" => [],
+      "availability_zone" => "nova",
+      "bootable" => "false",
+      "consistencygroup_id" => nil,
+      "created_at" => "2018-11-29T06:50:07.770785",
+      "description" => "description",
+      "encrypted" => false,
+      "id" => "f7223234-1afc-4d19-bfa3-d19deb6235ef",
+      "links" => [
+        {
+          "href" => "http://127.0.0.1:45839/v3/89afd400-b646-4bbc-b12b-c0a4d63e5bd3/volumes/f7223234-1afc-4d19-bfa3-d19deb6235ef",
+          "rel" => "self"
+        },
+        {
+          "href" => "http://127.0.0.1:45839/89afd400-b646-4bbc-b12b-c0a4d63e5bd3/volumes/f7223234-1afc-4d19-bfa3-d19deb6235ef",
+          "rel" => "bookmark"
+        }
+      ],
+      "metadata" => {},
+      "migration_status" => nil,
+      "multiattach" => false,
+      "name" => "cinder",
+      "os-vol-host-attr:host" => "test",
+      "os-vol-mig-status-attr:migstat" => nil,
+      "os-vol-mig-status-attr:name_id" => nil,
+      "os-vol-tenant-attr:tenant_id" => "89afd400-b646-4bbc-b12b-c0a4d63e5bd3",
+      "replication_status" => nil,
+      "size" => 10,
+      "snapshot_id" => nil,
+      "source_volid" => nil,
+      "status" => "creating",
+      "updated_at" => nil,
+      "user_id" => "c853ca26-e8ea-4797-8a52-ee124a013d0e",
+      "volume_type" => "__DEFAULT__",
+      "volume_image_metadata" => {}
     }
 
     volume = Yao::Volume.new(params)
+    assert_equal(volume.attachments, [])
+    assert_equal(volume.bootable?, false)
+    assert_equal(volume.description, "description")
+    assert_equal(volume.encrypted?, false)
+    assert_equal(volume.host, "test")
+    assert_equal(volume.metadata, {})
+    assert_equal(volume.migration_status, nil)
+    assert_equal(volume.multiattach?, false)
     assert_equal(volume.name, 'cinder')
     assert_equal(volume.size, 10)
+    assert_equal(volume.status, "creating")
+    assert_equal(volume.type, "__DEFAULT__")
+    assert_equal(volume.volume_image_metadata, {})
+    assert_equal(volume.volume_type, "__DEFAULT__")
   end
 
   def test_list
