cf03ec9a0d5f82e39e9d62ce7ddf5c7dfe903f07
Yuki Koya <ykky@pepabo.com>
2021-12-10 15:57:59 +0900

    Add methods

--- lib/yao/resources/volume.rb
+++ lib/yao/resources/volume.rb
@@ -1,7 +1,9 @@
 module Yao::Resources
   class Volume < Base
-    friendly_attributes :name, :size, :volume_type
+    friendly_attributes :attachments, :availability_zone, :bootable, :descriptions, :encrypted, :metadata, :multiattach, :name, :replication_status, :size, :snapshot_id, :status, :user_id, :volume_type
+    alias :type :volume_type
 
+    map_attribute_to_attribute 'os-vol-host-attr:host' => :host
     map_attribute_to_attribute 'os-vol-tenant-attr:tenant_id' => :tenant_id
 
     self.service        = "volumev3"
--- test/yao/resources/test_volume.rb
+++ test/yao/resources/test_volume.rb
@@ -1,13 +1,34 @@
 class TestVolume < TestYaoResource
   def test_volume
     params = {
+        'attachments' => [],
+        'availability_zone' => 'test',
+        'bootable' => true,
+        'encrypted' => false,
+        'metadata' => {},
+        'multiattach' => false,
         'name' => 'cinder',
-        'size' => 10
+        'replication_status' => 'disabled',
+        'size' => 10,
+        'snapshot_id' => nil,
+        'status' => 'available',
+        'user_id' => 'aaaa166533fd49f3b11b1cdce2430000',
+        'volume_type' => 'test'
     }
 
     volume = Yao::Volume.new(params)
+    assert_equal(volume.attachments, [])
+    assert_equal(volume.availability_zone, 'test')
+    assert_equal(volume.bootable, true)
+    assert_equal(volume.encrypted, false)
+    assert_equal(volume.metadata, {})
+    assert_equal(volume.multiattach, false)
     assert_equal(volume.name, 'cinder')
+    assert_equal(volume.replication_status, 'disabled')
     assert_equal(volume.size, 10)
+    assert_equal(volume.snapshot_id, nil)
+    assert_equal(volume.user_id, 'aaaa166533fd49f3b11b1cdce2430000')
+    assert_equal(volume.volume_type, 'test')
   end
 
   def test_list
