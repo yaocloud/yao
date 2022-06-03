681f4d51565a860cefecc074f80ec399e761ce91
Yuki Koya <ykky@pepabo.com>
2022-06-03 15:03:07 +0900

    Add enable and disable method

--- lib/yao/resources/volume_services.rb
+++ lib/yao/resources/volume_services.rb
@@ -7,7 +7,70 @@ module Yao::Resources
     map_attributes_to_time :updated_at
 
     self.service        = "volumev3"
-    self.resource_name  = "services"
-    self.resources_name = "services"
+    self.resource_name  = "os-service"
+    self.resources_name = "os-services"
+
+    # return true if ComputeServices is enabled
+    # @return [Bool]
+    def enabled?
+      status == 'enabled'
+    end
+
+    # return true if ComputeServices is disabled
+    # @return [Bool]
+    def disabled?
+      status == 'disabled'
+    end
+
+    def enable
+      self.class.enable(host, binary)
+    end
+
+    def disable(reason = nil)
+      self.class.disable(host, binary, reason)
+    end
+
+    class << self
+      # @param host [String]
+      # @param binary [String]
+      # @return [Hash]
+      def enable(host, binary)
+        params = {
+          "host" => host,
+          "binary" => binary,
+        }
+        put("enable", params)
+      end
+
+      # @param host [String]
+      # @param binary [String]
+      # @param resason [String]
+      # @return [Hash]
+      def disable(host, binary, reason = nil)
+        params = {
+          "host" => host,
+          "binary" => binary,
+        }
+        if reason
+          params["disabled_reason"] = reason
+          put("disable-log-reason", params)
+        else
+          put("disable", params)
+        end
+      end
+
+      private
+
+      # @param path [String]
+      # @param  params [Hash]
+      # @return [Hash]
+      def put(path, params)
+        res = PUT(create_url(path), params) do |req|
+          req.body = params.to_json
+          req.headers['Content-Type'] = 'application/json'
+        end
+        res.body
+      end
+    end
   end
 end
--- test/yao/resources/test_volume_services.rb
+++ test/yao/resources/test_volume_services.rb
@@ -1,4 +1,6 @@
 class TestVolumeServices < TestYaoResource
+  include RestfullyAccessibleStub
+
   def test_volume_services
     # https://docs.openstack.org/api-ref/block-storage/v3/index.html?expanded=list-all-cinder-services-detail#list-all-cinder-services
     params = JSON.parse(<<~JSON)
@@ -21,5 +23,94 @@ class TestVolumeServices < TestYaoResource
     assert_equal(Time.parse('2017-06-29T05:50:35.000000'), service.updated_at)
     assert_equal('devstack', service.host)
     assert_equal(nil, service.disabled_reason)
+  end
+
+  def test_enabled
+    service = Yao::VolumeServices.new({'status' => 'enabled'})
+    assert_equal(true, service.enabled?)
+  end
+
+  def test_disabled
+    service = Yao::VolumeServices.new({'status' => 'enabled'})
+    assert_equal(false, service.disabled?)
+  end
+
+  def stub_enable_request
+    body = {
+      "host" => "test-host",
+      "binary" => "test-binary",
+    }
+    response = { "disabled" => false }
+    stub_put_request('https://example.com:12345/os-services/enable', body, response)
+  end
+
+  def test_enable
+    stub = stub_enable_request
+    service = Yao::VolumeServices.new({
+      "host" => "test-host",
+      "binary" => "test-binary",
+    })
+    response = service.enable
+    assert_equal(false, response['disabled'])
+    assert_requested(stub)
+  end
+
+  def test_self_enable
+    stub = stub_enable_request
+    response = Yao::VolumeServices.enable('test-host', 'test-binary')
+    assert_equal(false, response['disabled'])
+    assert_requested(stub)
+  end
+
+  def stub_disable_request(reason = nil)
+    body = {
+      "host" => "test-host",
+      "binary" => "test-binary",
+    }
+    response = { "disabled" => true }
+    if reason
+      body["disabled_reason"] = reason
+      response["disabled_reason"] = reason
+      stub_put_request('https://example.com:12345/os-services/disable-log-reason', body, response)
+    else
+      stub_put_request('https://example.com:12345/os-services/disable', body, response)
+    end
+  end
+
+  def test_disable
+    stub = stub_disable_request
+    service = Yao::VolumeServices.new({
+      "host" => "test-host",
+      "binary" => "test-binary",
+    })
+    response = service.disable
+    assert_equal(true, response['disabled'])
+    assert_requested(stub)
+  end
+
+  def test_disable_reason
+    stub = stub_disable_request('test-reason')
+    service = Yao::VolumeServices.new({
+      "host" => "test-host",
+      "binary" => "test-binary",
+    })
+    response = service.disable('test-reason')
+    assert_equal(true, response['disabled'])
+    assert_equal('test-reason', response['disabled_reason'])
+    assert_requested(stub)
+  end
+
+  def test_self_disable
+    stub = stub_disable_request
+    response = Yao::VolumeServices.disable('test-host', 'test-binary')
+    assert_equal(true, response['disabled'])
+    assert_requested(stub)
+  end
+
+  def test_self_disable_reason
+    stub = stub_disable_request('test-reason')
+    response = Yao::VolumeServices.disable('test-host', 'test-binary', 'test-reason')
+    assert_equal(true, response['disabled'])
+    assert_requested(stub)
   end
 end
